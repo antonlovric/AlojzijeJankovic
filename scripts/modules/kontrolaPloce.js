5ae04911d6bae8ce1f063e0b686efa31e694ab38
Anton Lovrić <antonlovri81@gmail.com>
2021-11-23 17:06:22 +0100

    Reorganizirane scss datoteke, popravljeno scrollanje prilikom pregleda partija i poboljšana responzivnost na većini stranica

--- scripts/modules/kontrolaPloce.js
+++ scripts/modules/kontrolaPloce.js
@@ -16,12 +16,12 @@ let potez = 0;
 let brojPoteza;
 
 let brojStranice = 1;
-const tablica = document.querySelector("table").rows;
+const tablica = document.querySelector('table').rows;
 
-const prviPotez = document.querySelector("#prviPotez");
-const prethodniPotez = document.querySelector("#prethodniPotez");
-const iduciPotez = document.querySelector("#iduciPotez");
-const posljednjiPotez = document.querySelector("#posljednjiPotez");
+const prviPotez = document.querySelector('#prviPotez');
+const prethodniPotez = document.querySelector('#prethodniPotez');
+const iduciPotez = document.querySelector('#iduciPotez');
+const posljednjiPotez = document.querySelector('#posljednjiPotez');
 
 prviPotez.disabled = true;
 prethodniPotez.disabled = true;
@@ -29,16 +29,16 @@ iduciPotez.disabled = true;
 posljednjiPotez.disabled = true;
 
 document
-  .querySelector(".prethodnaStranica")
-  .addEventListener("click", function () {
+  .querySelector('.prethodnaStranica')
+  .addEventListener('click', function () {
     prikazStraniceTablice(--brojStranice);
   });
-document.querySelector(".iducaStranica").addEventListener("click", function () {
+document.querySelector('.iducaStranica').addEventListener('click', function () {
   prikazStraniceTablice(++brojStranice);
 });
 
 async function inicijalizirajPlocu() {
-  abChess = new AbChess("ploca", opcije);
+  abChess = new AbChess('ploca', opcije);
   abChess.setFEN();
   svePartije = await dohvatiPartije(abChess);
   napuniTablicu(svePartije);
@@ -47,10 +47,10 @@ async function inicijalizirajPlocu() {
 }
 
 function napuniTablicu(partije) {
-  const tijeloTablice = document.querySelector("#sadrzajTablice");
+  const tijeloTablice = document.querySelector('#sadrzajTablice');
   let redniBroj = 1;
   partije.forEach((partija) => {
-    const dijeloviPartije = partija.split("]");
+    const dijeloviPartije = partija.split(']');
     const bijeli = dijeloviPartije[4].substring(
       9,
       dijeloviPartije[4].length - 1
@@ -95,34 +95,33 @@ function napuniTablicu(partije) {
 }
 
 function inicijalizirajTablicu() {
-  let partije = document.querySelectorAll(".partijaUTablici");
+  let partije = document.querySelectorAll('.partijaUTablici');
   partije.forEach((partija) => {
-    partija.addEventListener("mouseover", function () {
-      this.classList.add("oznacenaPartija");
+    partija.addEventListener('mouseover', function () {
+      this.classList.add('oznacenaPartija');
     });
-    partija.addEventListener("mouseleave", function () {
-      this.classList.remove("oznacenaPartija");
+    partija.addEventListener('mouseleave', function () {
+      this.classList.remove('oznacenaPartija');
     });
-    partija.addEventListener("click", function () {
+    partija.addEventListener('click', function () {
       partije.forEach((partija) => {
-        if (partija.classList.contains("odabranaPartija")) {
-          partija.classList.remove("odabranaPartija");
+        if (partija.classList.contains('odabranaPartija')) {
+          partija.classList.remove('odabranaPartija');
         }
       });
 
-      this.classList.add("odabranaPartija");
+      this.classList.add('odabranaPartija');
       odaberiPartiju(this);
     });
   });
 }
 
 function odaberiPartiju(object) {
-  console.log("usao u odabir");
-  const paragraf = document.querySelector(".trenutnaPartija");
+  const paragraf = document.querySelector('.trenutnaPartija');
   let index = object.children[0].innerHTML - 1;
   let partija = informacijeOPartijama[index];
-  let crni = document.querySelector(".crni");
-  let bijeli = document.querySelector(".bijeli");
+  let crni = document.querySelector('.crni');
+  let bijeli = document.querySelector('.bijeli');
   crni.innerHTML = partija.crni;
   bijeli.innerHTML = partija.bijeli;
   potez = 0;
@@ -130,50 +129,48 @@ function odaberiPartiju(object) {
   abChess.setPGN(partija.pgn);
 
   let poteziPartije = abChess.getMovesPGN();
-  const tablicaPoteza = document.querySelector(".potezi");
-  tablicaPoteza.innerHTML = "";
+  const tablicaPoteza = document.querySelector('.potezi');
+  tablicaPoteza.innerHTML = '';
   let brojacPoteza = 1;
   for (let i = 0; i < poteziPartije.length; i += 2) {
     const sablona = `<tr class = "potezPartije" colspan = "3">
     <td>${brojacPoteza++}</td>
-    <td>${poteziPartije[i] == undefined ? "" : poteziPartije[i]}</td>
-    <td>${poteziPartije[i + 1] == undefined ? "" : poteziPartije[i + 1]}</td>
+    <td>${poteziPartije[i] == undefined ? '' : poteziPartije[i]}</td>
+    <td>${poteziPartije[i + 1] == undefined ? '' : poteziPartije[i + 1]}</td>
     </tr>`;
     tablicaPoteza.innerHTML += sablona;
   }
 
   brojPoteza = abChess.getMovesPGN().length;
-  console.log(brojPoteza);
   osvjeziGumbe();
   osvjeziPlocu();
-  document.querySelector(".pretinacPloce").style.display = "flex";
-  document.querySelector(".tekstIznadPloce").scrollIntoView();
+  document.querySelector('.pretinacPloce').style.display = 'flex';
+  document.querySelector('.tekstIznadPloce').scrollIntoView();
 }
 
 function prikazStraniceTablice(brojStranice) {
   const ukupniBrojStranica = svePartije.length;
 
-  const prikazBrojaStranice = document.querySelector(".brojStranice");
+  const prikazBrojaStranice = document.querySelector('.brojStranice');
   prikazBrojaStranice.innerHTML =
-    brojStranice + " / " + Math.round(ukupniBrojStranica / 10);
+    brojStranice + ' / ' + Math.round(ukupniBrojStranica / 10);
   let zadnjaPrikazanaPartija = brojStranice * 10;
   let prvaPrikazanaPartija = zadnjaPrikazanaPartija - 9;
 
   for (let i = 0; i < tablica.length; i++) {
     if (i < prvaPrikazanaPartija || i > zadnjaPrikazanaPartija) {
-      tablica[i].style.display = "none";
+      tablica[i].style.display = 'none';
     } else {
-      tablica[i].style.display = "table-row";
+      tablica[i].style.display = 'table-row';
     }
   }
-  tablica[0].style.display = "table-row";
+  tablica[0].style.display = 'table-row';
   osvjeziGumbeTablice();
 }
 
 function osvjeziGumbeTablice() {
-  const prethodnaStranica = document.querySelector(".prethodnaStranica");
-  const iducaStranica = document.querySelector(".iducaStranica");
-  console.log(brojStranice);
+  const prethodnaStranica = document.querySelector('.prethodnaStranica');
+  const iducaStranica = document.querySelector('.iducaStranica');
   if (brojStranice == 1) {
     prethodnaStranica.disabled = true;
   } else {
@@ -193,20 +190,23 @@ function osvjeziPlocu() {
 
 function obojiRedakPoteza(trenutniPotez, sviPotezi) {
   sviPotezi.forEach((potez) => {
-    console.log(potez);
     if (potez === trenutniPotez) {
-      potez.classList.add("trenutniRed");
+      potez.classList.add('trenutniRed');
 
-      potez.scrollIntoView();
+      potez.scrollIntoView({
+        behavior: 'smooth',
+        block: 'nearest',
+        inline: 'start',
+      });
     } else {
-      potez.classList.remove("trenutniRed");
+      potez.classList.remove('trenutniRed');
     }
   });
 }
 
 function oznaciPotez() {
   let trenutniRed = Math.round(potez / 2) - 1;
-  const sviPotezi = document.querySelectorAll(".potezPartije");
+  const sviPotezi = document.querySelectorAll('.potezPartije');
   if (sviPotezi[trenutniRed] != undefined) {
     obojiRedakPoteza(sviPotezi[trenutniRed], sviPotezi);
   }
@@ -253,19 +253,19 @@ function igrajPosljednjiPotez() {
 }
 
 if (iduciPotez != undefined) {
-  prviPotez.addEventListener("click", igrajPrviPotez);
-  prethodniPotez.addEventListener("click", igrajPrethodniPotez);
-  iduciPotez.addEventListener("click", igrajIduciPotez);
-  posljednjiPotez.addEventListener("click", igrajPosljednjiPotez);
+  prviPotez.addEventListener('click', igrajPrviPotez);
+  prethodniPotez.addEventListener('click', igrajPrethodniPotez);
+  iduciPotez.addEventListener('click', igrajIduciPotez);
+  posljednjiPotez.addEventListener('click', igrajPosljednjiPotez);
 }
 
 async function dohvatiPartije(abChess) {
   const partije = await preuzmiPartije();
-  return partije.split("¤");
+  return partije.split('¤');
 }
 
 function preuzmiPartije() {
-  return fetch("../misc/master_games.pgn").then((odgovor) =>
+  return fetch('../misc/master_games.pgn').then((odgovor) =>
     odgovor.text().then((dohvacenePartije) => {
       return dohvacenePartije;
     })
